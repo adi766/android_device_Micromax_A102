1ae1890de597d136f058805bf7215aa2f0cad026
tribetmen <dima.koloskov.1997@yandex.ru>
2017-05-10 17:37:00 +0300

    Fix FM

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -159,13 +159,3 @@ POLICYVERS := 26
 
 # Hack for build
 $(shell mkdir -p $(OUT)/obj/KERNEL_OBJ/usr)
-
-# FMRadio
-MTK_FM_SUPPORT := yes
-MTK_FM_RX_SUPPORT := yes
-MTK_FM_CHIP := MT6627_FM
-FM_LIB_BUILD_MT6620 := yes
-FM_LIB_BUILD_MT6627 := yes
-FM_LIB_BUILD_MT6628 := yes
-FM_LIB_BUILD_MT6630 := yes
-
